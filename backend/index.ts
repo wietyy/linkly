62d49da738b43cf0c5d8508562ee03b2e203b175
pxlmastrXD <calebwietholter9@outlook.com>
2026-05-17 19:18:49 -0400

    completed index.ts (for now), db.ts outputs boilerplate stuff now

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
+import { env } from "node:process";
+import { createLink, getLink } from "./lib/db";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = env.PORT || 3000;
 
 app.use(express.json());
 
-// TODO: hook this up to db.ts
-app.get("/:linkId", (req: any, res: any) => {
-
+// Working (5/17/26)
+app.get("/:linkId", async (req: any, res: any) => {
+    let linkId = req.params.linkId;
+    let link: string = await getLink(linkId);
+    res.redirect(link);
 })
 
-// TODO: hook this up to db.ts
-app.post("/api/createLink", (req: any, res: any) => {
-
+// Working (5/17/26)
+app.post("/api/createLink", async (req: any, res: any) => {
+    const {requestedID, link} = req.body;
+    const linkId = await createLink(requestedID, link);
+    res.send(linkId);
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is running on port 3000");
 });
--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -1,11 +1,16 @@
 // TODO: link up to supabase
 
 // TODO: Create this function
-async function createLink() {
-
+async function createLink(linkId: string, link: string): Promise<string> {
+    return "123456"; // remove later
 }
 
 // TODO: Create this function
-async function getLink() {
+async function getLink(linkId: string): Promise<string> {
+    return "https://www.google.com"; // remove later
+}
 
+export {
+    createLink,
+    getLink
 }
